b56322f2f89a4a92bee1e1be989feb251b59b9ec
mhd1991 <m91.alahmadi@gmail.com>
2014-09-13 14:09:28 -0700

    added function

--- js/app.js
+++ js/app.js
@@ -8,18 +8,21 @@ $(document).ready(function() {
 			$('div').children('p').remove();
 			limit = $(this).val();
 			$(this).val('');
-			for (var i=0; i<limit; i++) {
-				if ((i%3!=0) && (i%5!=0)) {
-					$('div').append('<p>'+i+'</p>');
-				} else if ((i%3==0) && (i%5!=0)) {
-					$('div').append('<p class="fizz">fizz</p>');
-				} else if ((i%3!=0) && (i%5==0)) {
-					$('div').append('<p class="fuzz">buzz</p>');
-				} else if ((i%3==0) && (i%5==0)) {
-					$('div').append('<p class="fizzfuzz">fizzbuzz</p>');
-				}
-			}
+			fizzbuzz(limit);
 		}
 	});
-
 });
+
+var fizzbuzz = function(limit){
+	for (var i=0; i<limit; i++) {
+		if ((i%3!=0) && (i%5!=0)) {
+			$('div').append('<p>'+i+'</p>');
+		} else if ((i%3==0) && (i%5!=0)) {
+			$('div').append('<p class="fizz">fizz</p>');
+		} else if ((i%3!=0) && (i%5==0)) {
+			$('div').append('<p class="fuzz">buzz</p>');
+		} else if ((i%3==0) && (i%5==0)) {
+			$('div').append('<p class="fizzfuzz">fizzbuzz</p>');
+		}
+	}
+}
